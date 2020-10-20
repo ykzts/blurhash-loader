d70501e0ebd1544fc29b2e4d65d216babf85019a
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-10-20 19:45:14 +0900

    feat(loader): pass original content

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import { getOptions } from 'loader-utils'
 import { loader } from 'webpack'
 import Jimp from './jimp'
 
+const EXPORT_RE = /(?:export\s+default|module.exports\s+=)(?:\s|$)|/
+
 type LoaderOptions = {
   componentX?: number
   componentY?: number
   esModule?: boolean
 }
 
-function loader(this: loader.LoaderContext, content: Buffer): void {
+function loader(this: loader.LoaderContext, rawContent: Buffer | string): void {
   if (this.cacheable) this.cacheable()
 
   const callback = this.async()
@@ -17,7 +19,11 @@ function loader(this: loader.LoaderContext, content: Buffer): void {
   const esModule =
     typeof options.esModule !== 'undefined' ? options.esModule : true
 
-  Jimp.read(content)
+  const content = Buffer.isBuffer(rawContent)
+    ? rawContent.toString('utf8')
+    : rawContent
+
+  Jimp.read(this.resourcePath)
     .then(({ bitmap }) => {
       const pixels = new Uint8ClampedArray(bitmap.data)
       const blurhash = encode(
@@ -28,12 +34,17 @@ function loader(this: loader.LoaderContext, content: Buffer): void {
         options.componentY || 3
       )
 
-      callback(
-        null,
-        `${esModule ? 'export default' : 'module.exports ='} ${JSON.stringify(
-          blurhash
-        )}`
-      )
+      let result = ''
+
+      if (EXPORT_RE.test(content)) {
+        result += `${content}\n\n`
+      }
+
+      result += `${
+        esModule ? 'export const ' : 'exports.'
+      }blurhash = ${JSON.stringify(blurhash)}\n`
+
+      callback(null, result)
     })
     .catch((error) => {
       callback(error, null)
