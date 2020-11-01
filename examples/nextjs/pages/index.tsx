11a93c9792c2489114ceeb8425d7e5ffb840b555
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-11-02 00:11:59 +0900

    chore(example): use image component

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import { BlurhashCanvas } from 'react-blurhash'
 import photo, { blurhash } from '../photos/ramen.jpg'
 import styles from '../styles/Home.module.css'
@@ -28,8 +29,23 @@ export default function Home() {
         </p>
 
         <div className={styles.photo}>
-          <img alt="" height={600} src={photo} width={800} />
-          <BlurhashCanvas hash={blurhash} height={600} width={800} />
+          <div className={styles.imageWrapper}>
+            <Image
+              alt=""
+              height={600}
+              sizes="(max-width: 360px) 100vw, 800px"
+              src={photo}
+              width={800}
+            />
+          </div>
+          <div className={styles.blurhashWrapper}>
+            <BlurhashCanvas
+              className={styles.blurhash}
+              hash={blurhash}
+              height={600}
+              width={800}
+            />
+          </div>
         </div>
       </main>
 
