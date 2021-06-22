2199579ba1c18a07f6c12615850084699b542dbc
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-06-22 09:31:06 +0900

    fix(website): disable eslint

--- website/next.config.js
+++ website/next.config.js
@@ -1,3 +1,7 @@
 const withNextra = require('nextra')('nextra-theme-docs', './theme.config.tsx')
 
-module.exports = withNextra()
+module.exports = withNextra({
+  eslint: {
+    ignoreDuringBuilds: true
+  }
+})
